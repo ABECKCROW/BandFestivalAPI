aed746ab164ec0f65bac59efde59f49e9b10289f
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-08-25 01:15:52 +0900

    レスポンスがnullになってしまう。

--- src/main/java/com/lesson9/Bandlist/BandMapper.java
+++ src/main/java/com/lesson9/Bandlist/BandMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import javax.xml.crypto.Data;
 import java.util.List;
 
 @Mapper
@@ -15,9 +16,12 @@ public interface BandMapper {
     @Select("SELECT * FROM bands WHERE id = #{id}")
     Band findById(int id);
 
-    @Insert("INSERT INTO bands (name) VALUES (#{name})")
+    @Insert("INSERT INTO bands (band_name, act_Announcement_Date) VALUES (#{band_name}, #{act_Announcement_Date})")
     void create(Band band);
 
-    @Update("UPDATE bands SET name = #{name} WHERE id = #{id}")
+    @Update("UPDATE bands SET band_name = #{name} WHERE id = #{id}")
     void update(Band band);
+
+//    @Update("UPDATE bands SET act_Announcement_Date = #{date} WHERE id = #{id}")
+//    void update(Data data);
 }
