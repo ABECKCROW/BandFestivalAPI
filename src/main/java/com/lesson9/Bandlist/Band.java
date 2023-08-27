313868f2265d75a0ae3681ce57850ee91a128f9c
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-08-28 01:10:27 +0900

    Bandクラスのsetterを消去

--- src/main/java/com/lesson9/Bandlist/Band.java
+++ src/main/java/com/lesson9/Bandlist/Band.java
@@ -2,13 +2,11 @@ package com.lesson9.Bandlist;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.time.ZonedDateTime;
 
 @Getter
-@Setter
 @ToString
 @AllArgsConstructor
 public class Band {
