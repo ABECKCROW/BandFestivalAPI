efda1ffce04017c6930be69780674d4ffce7c1e6
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-10-13 23:34:16 +0900

    ID指定正常のテスト

---
+++ src/test/java/com/lesson9/Bandlist/service/BandServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.lesson9.Bandlist.service;
+
+import com.lesson9.Bandlist.entity.Band;
+import com.lesson9.Bandlist.mapper.BandMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class BandServiceImplTest {
+
+    @InjectMocks
+    BandServiceImpl bandServiceImpl;
+
+    @Mock
+    BandMapper bandMapper;
+
+    @Test
+    public void バンド検索で存在するIDを指定した時に正常にバンドが返されること() throws Exception {
+        ZonedDateTime date = ZonedDateTime.of(2023, 12, 1, 0, 0, 0, 0, ZoneId.of("UTC"));
+        doReturn(Optional.of(new Band(1, "ASIAN KUNG-FU GENERATION", date))).when(bandMapper).findById(1);
+
+        Optional<Band> actual = bandServiceImpl.findById(1);
+        assertThat(actual).isEqualTo(new Band(1, "ASIAN KUNG-FU GENERATION", date));
+        verify(bandMapper, times(1)).findById(1);
+    }
+}
