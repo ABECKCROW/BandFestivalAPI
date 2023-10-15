6277d6c9ecb5fb5b9903422a5461ef36b534b1e2
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-10-16 03:26:58 +0900

    NotFoundExceptionクラスの実装

---
+++ src/main/java/com/lesson9/Bandlist/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.lesson9.Bandlist.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
