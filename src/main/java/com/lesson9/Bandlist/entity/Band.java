09e1fd83118a208e87376970d810d4da0dca046d
ABECKCROW <mudskippermat@yahoo.co.jp>
2023-10-15 10:17:46 +0900

    LombokでequalsとhashCodeを生成

--- src/main/java/com/lesson9/Bandlist/entity/Band.java
+++ src/main/java/com/lesson9/Bandlist/entity/Band.java
@@ -1,38 +1,20 @@
 package com.lesson9.Bandlist.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.ZonedDateTime;
-import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Band {
     private int id;
     private String bandName;
     private ZonedDateTime actAnnouncementDate;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Band band = (Band) o;
-        return id == band.id &&
-                Objects.equals(bandName, band.bandName) &&
-                Objects.equals(actAnnouncementDate, band.actAnnouncementDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, bandName, actAnnouncementDate);
-    }
 }
